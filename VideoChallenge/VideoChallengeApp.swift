8c2f5c50ae6d1c8b201d9cac1f4bfd0bfaf111ad
Arthur <olehno.arturs@gmail.com>
2023-01-24 00:28:04 +0200

    Initial Commit

---
+++ VideoChallenge/VideoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoChallengeApp.swift
+//  VideoChallenge
+//
+//  Created by Артур Олехно on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
